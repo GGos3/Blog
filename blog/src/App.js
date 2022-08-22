645575f2526e43800c436a0c3074740ac410f6e0
JangFeel <janpll0421@gmail.com>
2022-08-22 14:57:23 +0900

    FEAT: 글 작성, 삭제 기능 추가
    stopPropagation 함수를 이용해 이벤트 더블링 방지

--- blog/src/App.js
+++ blog/src/App.js
@@ -13,6 +13,7 @@ function App() {
   let [좋아요, 좋아요변경] = useState([0, 0, 0]);
   let [modal, setModal] = useState(false);
   let [index, setindex] = useState(0);
+  let [입력값, 입력값변경] = useState("");
 
   return (
     <div className="App">
@@ -49,7 +50,8 @@ function App() {
             >
               {글제목[i]}{" "}
               <span
-                onClick={() => {
+                onClick={(e) => {
+                  e.stopPropagation(); //이벤트 더블링 방지
                   let copy = [...좋아요];
                   copy[i] = copy[i] + 1;
                   좋아요변경(copy);
@@ -60,9 +62,33 @@ function App() {
               {좋아요[i]}
             </h4>
             <p>2월 17일 발행</p>
+            <button
+              onClick={() => {
+                let copy = [...글제목];
+                copy.splice(i, 1);
+                글제목변경(copy);
+              }}
+            >
+              삭제
+            </button>
           </div>
         );
       })}
+
+      <input
+        onChange={(e) => {
+          입력값변경(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          let copy = [...글제목];
+          copy.unshift(입력값);
+          글제목변경(copy);
+        }}
+      >
+        글 발행
+      </button>
 
       {modal == true ? (
         <Modal
